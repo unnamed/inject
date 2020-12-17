0b1762c5d316018f753269df0d44a7432468314f
yushu@unnamed.team <yushu@unnamed.team>
2020-12-17 00:18:21 -0500

    :white_check_mark: Add exploring tests

---
+++ core/src/test/java/me/yushust/inject/explore/ProxiedFactoryProviderExploreTest.java
@@ -0,0 +1,45 @@
+package me.yushust.inject.explore;
+
+import me.yushust.inject.Injector;
+import me.yushust.inject.assisted.Assist;
+import me.yushust.inject.assisted.Assisted;
+import me.yushust.inject.assisted.ValueFactory;
+import me.yushust.inject.assisted.provision.ProxiedFactoryProvider;
+import me.yushust.inject.key.Key;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Provider;
+
+public class ProxiedFactoryProviderExploreTest {
+
+  @Test
+  public void test() {
+
+    Injector injector = Injector.create(binder -> binder.bind(Foo.class).toFactory(FooFactory.class));
+    Provider<? extends FooFactory> provider = injector.getProvider(FooFactory.class);
+    ProxiedFactoryProvider<?> proxiedProvider = (ProxiedFactoryProvider<?>) provider;
+
+    Assertions.assertEquals(Key.of(Foo.class), proxiedProvider.getBuildType());
+    Assertions.assertEquals(FooFactory.class, proxiedProvider.getFactory());
+    Assertions.assertEquals("create", proxiedProvider.getFactoryMethod().getName());
+  }
+
+  public static class Foo {
+
+    @Assisted
+    public Foo(
+        @Assist String name,
+        Object empty
+    ) {
+    }
+
+  }
+
+  public interface FooFactory extends ValueFactory {
+
+    Foo create(String name);
+
+  }
+
+}
