67411093e338fd75cec8b17fb13aaf4ad40f6b47
yusshu <yushu@unnamed.team>
2021-05-20 16:48:09 -0500

    tests: improve provider-methods test class

--- core/src/test/java/me/yushust/inject/ProviderMethodsTest.java
+++ core/src/test/java/me/yushust/inject/ProviderMethodsTest.java
@@ -10,19 +10,48 @@ import java.util.UUID;
 
 public class ProviderMethodsTest {
 
+  public interface Component1 { }
+  public static class Component1Impl implements Component1 { }
+  public static class Component1Impl2 implements Component1 { }
+
+  public interface Component2 { }
+  public static class Component2Impl implements Component2 { }
+  public static class Component2Impl2 implements Component2 { }
+
   public static class MyModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+      bind(Component1.class).to(Component1Impl.class).singleton();
+      bind(Component1.class).named("2").to(Component1Impl2.class).singleton();
+
+      bind(Component2.class).to(Component2Impl.class).singleton();
+      bind(Component2.class).named("2").to(Component2Impl2.class).singleton();
+    }
+
     @Provides
-    public String provideName() {
+    public String provideName(Component1 component1, Component2 component2) {
+      Assertions.assertNotNull(component1);
+      Assertions.assertNotNull(component2);
       return "trew";
     }
 
     @Provides @Named("hello")
-    public String provideHello() {
+    public String provideHello(@Named("2") Component1 component1, @Named("2") Component2 component2) {
+      Assertions.assertNotNull(component1);
+      Assertions.assertNotNull(component2);
       return "hello";
     }
 
+    @Provides @Named("author")
+    public String provideAuthor(String name) {
+      Assertions.assertEquals("trew", name);
+      return "yusshu";
+    }
+
     @Provides @Singleton
-    public UUID provideId() {
+    public UUID provideId(@Named("author") String author) {
+      Assertions.assertEquals("yusshu", author);
       return UUID.randomUUID();
     }
   }
@@ -33,7 +62,12 @@ public class ProviderMethodsTest {
     Foo foo = injector.getInstance(Foo.class);
     Assertions.assertEquals("trew", foo.trew);
     Assertions.assertEquals("hello", foo.hello);
+    Assertions.assertEquals("yusshu", foo.author);
     Assertions.assertSame(foo.id1, foo.id2);
+    Assertions.assertNotNull(foo.component1);
+    Assertions.assertNotNull(foo.component2);
+    Assertions.assertNotNull(foo.component12);
+    Assertions.assertNotNull(foo.component22);
   }
 
   public static class Foo {
@@ -41,6 +75,11 @@ public class ProviderMethodsTest {
     @Inject @Named("hello") private String hello;
     @Inject private UUID id1;
     @Inject private UUID id2;
+    @Inject @Named("author") private String author;
+    @Inject private Component1 component1;
+    @Inject private Component2 component2;
+    @Inject @Named("2") private Component1 component12;
+    @Inject @Named("2") private Component2 component22;
   }
 
 }
