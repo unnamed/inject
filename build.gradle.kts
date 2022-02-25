3e08a992251b9ccac3e6634e17b3903090b401b0
yusshu <yushu@unnamed.team>
2022-02-24 23:24:51 -0500

    build: specify junit-jupiter-engine version

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     api("javax.inject:javax.inject:1")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 tasks {
