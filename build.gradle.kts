ff8ee4e9ed9007cd1769c007a2b8047ccb8c89d2
yusshu <yushu@unnamed.team>
2022-04-08 18:27:48 -0500

    build: force java 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,12 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
+}
+
 tasks {
     test {
         useJUnitPlatform()
