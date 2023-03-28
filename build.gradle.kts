56a5c1dbc129545973719b0eaacc12a87930f466
yusshu <yushuverduras1309@gmail.com>
2023-03-28 18:36:15 -0500

    deps: remove javax.inject dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,6 @@ repositories {
 }
 
 dependencies {
-    api("javax.inject:javax.inject:1")
-
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
